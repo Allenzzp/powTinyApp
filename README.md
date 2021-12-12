3542dbd8840f1011ead0ea118a678503061d971a
Allenzzp <131452abc@gmail.com>
2021-12-12 04:46:54 +0000

    implement create new urls

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # powTinyApp
-Refacotor tinyApp project and add features
+Refactor tinyApp project and add features
+
+Routes are ordered from most specific to least specific
 
 ## To Do Lists
 - [ ] Implement all features as required by tinyApp project
 - [ ] Implement the Unique visitors tracking feature --- set up additional cookie
 - [ ] Allow users to generate a QR code based on given longURLs
 
-## App Structure
+## Route Logic
 
-- /urls display all URLs and shortened forms
-- /urls/:shortURL displays a single URL and its shortened form
-- 
+- GET /urls --- display all URLs and shortened forms a user created
+- GET /urls/:shortURL --- displays a short/long URL pair
+- GET /urls/new --- allows user to create a new shortURL by entering a longURL 
+- urls_new.ejs submit button --- send post request to /urls
+- POST /urls --- create a new short/long URL pair --- redirect to urls/:shortURL
+- GET /u/:shortURL --- redirect to corresponding longURL
+
--- express_server.js
+++ express_server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 const urlsDB = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,6 +19,33 @@ const usersDB = {
     password: "2690" 
   }
 };
+//Helper functions
+const generateRandomString = () => {
+  const letterList = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const generatedArray = [];
+  let n = 1;
+  while (n <= 6) {
+    const index = Math.floor(Math.random() * 62);
+    generatedArray.push(letterList[index]);
+    n++;
+  }
+  return generatedArray.join("");
+};
+const completeURL = (url) => {
+  let compltURL = url;
+  const head1 = "http://";
+  const head2 = "www.";
+  if (!url.includes(head2)) {
+    compltURL = head2 + compltURL;
+  }
+  if (!url.includes(head1)) {
+    compltURL = head1 + compltURL;
+  }
+  return compltURL;
+};
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
@@ -31,6 +63,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -39,6 +75,17 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlsDB[req.params.shortURL];
+  return res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+  const longURL = completeURL(req.body.longURL);
+  const shortURL = generateRandomString();
+  urlsDB[shortURL] = longURL;
+  return res.redirect(`/urls/${shortURL}`);
+});
 
 
 app.get("/hello", (req, res) => {
